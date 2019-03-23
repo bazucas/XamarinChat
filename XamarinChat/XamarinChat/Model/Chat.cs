c433ca0d3be1289d9c826b88a204e23aa0d73bd0
Luis Inacio <lrs.inacio@gmail.com>
2019-03-23 22:14:47 +0000

    Structure

---
+++ XamarinChat/XamarinChat/Model/Chat.cs
@@ -0,0 +1,8 @@
+﻿namespace XamarinChat.Model
+{
+    public class Chat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ XamarinChat/XamarinChat/Model/Message.cs
@@ -0,0 +1,11 @@
+﻿namespace XamarinChat.Model
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int IdChat { get; set; }
+        public int IdUser { get; set; }
+        public string Msg { get; set; }
+        public User User { get; set; }
+    }
+}
